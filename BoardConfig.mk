7504ee92cd18ecfd48ca4e91860a6ba42217b795
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    whyred: Enforce common device & vendor tree presence
    A compiled build has no chance of booting without the common device setup
    and the prebuilt binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@
 #
 
 # Inherit from sdm660-common
--include device/xiaomi/sdm660-common/BoardConfigCommon.mk
+include device/xiaomi/sdm660-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/whyred
 
@@ -56,4 +56,4 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 VENDOR_SECURITY_PATCH := 2018-11-01
 
 # Inherit the proprietary files
--include vendor/xiaomi/whyred/BoardConfigVendor.mk
+include vendor/xiaomi/whyred/BoardConfigVendor.mk
