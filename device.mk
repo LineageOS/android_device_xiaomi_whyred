838b655c359ea848a0d7f284bc6f352f78357cd6
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 00:49:11 +0300

    whyred: Add powerhint.xml
     * Based on LA.UM.6.2.r1-08700-sdm660.0
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ief38dc242a92de05305452634fe2318e4de42f90
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- device.mk
+++ device.mk
@@ -230,6 +230,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Properties
 include device/xiaomi/whyred/vendor_prop.mk
 
