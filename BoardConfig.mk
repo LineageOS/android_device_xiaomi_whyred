36c71595537eee44fc2911348cbdf37567ee6002
Vasishath Kaushal <vasishath@gmail.com>
2018-06-04 22:26:13 +0530

    whyred: Initialise board variables

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Board device path
+DEVICE_PATH := device/xiaomi/whyred
+
+BOARD_VENDOR := xiaomi
+
+TARGET_BOOTLOADER_BOARD_NAME := sdm636
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno509
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := sdm660
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+
+
+# Inherit from proprietary files
+include vendor/xiaomi/whyred/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -13,3 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+
+# Call the proprietary setup
+$(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
