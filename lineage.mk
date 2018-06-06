dcf70ff652318b5aa4ed092dbf65f6ac9db79e98
Shahan-mik3 <shahan.mike13@gmail.com>
2018-06-06 22:53:29 +0530

    whyred: Add lineage_whyred target

---
+++ lineage.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := whyred
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+# Inherit from whyred device
+$(call inherit-product, device/xiaomi/whyred/device.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.controller=a800000.dwc3 \
+    sys.usb.rndis.func.name=rndis_bam \
+    sys.usb.rmnet.func.name=rmnet_bam
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_whyred
+PRODUCT_DEVICE := whyred
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 5 Pro
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
+TARGET_VENDOR_PRODUCT_NAME := whyred
+TARGET_VENDOR_DEVICE_NAME := whyred
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=whyred PRODUCT_NAME=whyred
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 8.4.27 release-keys"
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/8.4.27:user/release-keys
