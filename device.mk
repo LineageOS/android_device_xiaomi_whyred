c35da150b942e1e42fff3cd809616d15915a994d
guaiyihu <guaiyihu@foxmail.com>
2018-06-08 13:11:25 +0800

    whyred: rootdir: Clean up unused init scripts
     * init.qcom.class_core.sh
     * init.qcom.coex.sh
     * init.qcom.crashdata.sh
     * init.qcom.efs.sync.sh
     * init.qcom.sdio.sh
     * init.qcom.syspart_fixup.sh
     * init.qcom.wifi.sh
     * init.qti.ims.sh
     * qca6234-service.sh
    
    Change-Id: Ib6ba59acb1f1d6b773aeca3c326944049784887c

--- device.mk
+++ device.mk
@@ -272,16 +272,9 @@ PRODUCT_PACKAGES += \
     init.crda.sh \
     init.mdm.sh \
     init.qcom.class_core.sh \
-    init.qcom.coex.sh \
-    init.qcom.crashdata.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
-    init.qcom.sdio.sh \
-    init.qcom.syspart_fixup.sh \
     init.qcom.usb.sh \
-    init.qcom.wifi.sh \
     init.qti.fm.sh \
-    init.qti.ims.sh \
     init.goodix.sh \
     ueventd.rc
 
