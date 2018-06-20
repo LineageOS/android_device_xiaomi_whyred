9874ad2f3a6841b0d336b664770b555500ae8fff
Vasishath Kaushal <vasishath@gmail.com>
2018-06-21 00:14:01 +0530

    whyred: Add graphics mapper HIDL

--- treble.mk
+++ treble.mk
@@ -36,4 +36,5 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.graphics.mapper@2.0-impl
