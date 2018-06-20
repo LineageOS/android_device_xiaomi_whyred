ead0a0c4f5903051f6e50a8d16e05fc8b187d358
Vasishath Kaushal <vasishath@gmail.com>
2018-06-21 00:18:37 +0530

    whyred: Add audio broadcastradio HIDL

--- treble.mk
+++ treble.mk
@@ -44,4 +44,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl
