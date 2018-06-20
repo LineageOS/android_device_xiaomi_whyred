b393c2eefb96e623b0b864acd8961ffa34c25514
Vasishath Kaushal <vasishath@gmail.com>
2018-06-21 00:13:21 +0530

    whyred: Add memtrack HIDL

--- treble.mk
+++ treble.mk
@@ -34,4 +34,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.composer@2.1-impl
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl
