a399e587dba43279cfce00e00e0086c6e334366c
Vasishath Kaushal <vasishath@gmail.com>
2018-06-21 00:12:40 +0530

    whyred: Add hwcomposer HIDL

--- treble.mk
+++ treble.mk
@@ -32,4 +32,6 @@ PRODUCT_PACKAGES += \
 #Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@2.0-impl
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl
