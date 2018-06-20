a5c1b04cb695c893b9e49e37e3353f69d6c59b2f
Vasishath Kaushal <vasishath@gmail.com>
2018-06-21 00:18:02 +0530

    whyred: Add audio effect HIDL

--- treble.mk
+++ treble.mk
@@ -43,4 +43,5 @@ PRODUCT_PACKAGES += \
 #Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@2.0-impl
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
