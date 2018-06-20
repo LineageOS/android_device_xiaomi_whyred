9a8801d8c2ec5dc403dc464fb1418ad095f6f3d1
Vasishath Kaushal <vasishath@gmail.com>
2018-06-21 00:19:18 +0530

    whyred: Add soundtrigger HIDL HAL

--- treble.mk
+++ treble.mk
@@ -45,4 +45,5 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
