7e2db45ce6950d7c59f07e95103955cb58ca72a0
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    whyred: Remove camera.device@3.2-impl from PRODUCT_PACKAGES
    * camera.device@3.2-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- treble.mk
+++ treble.mk
@@ -24,7 +24,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
-    camera.device@3.2-impl \
     camera.device@3.3-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
