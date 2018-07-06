cd4f93524de33c5c5f6cbd2fb7ba082a9d623e68
Subhaam <derksoul98@gmail.com>
2018-07-06 21:33:04 +0530

    whyred: import qti privileged app permissions from MIUI 9.5.13
    Change-Id: I6b95e9dd03add8c779f9c4a70a8f3714b4fdfc63
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- device.mk
+++ device.mk
@@ -301,6 +301,7 @@ PRODUCT_PACKAGES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # Connectivity Engine support (CNE)
