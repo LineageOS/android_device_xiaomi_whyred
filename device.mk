865eae00a92b4211769187edecefea12e36bca27
Abhay Ruparel <abhayruparel2000@gmail.com>
2018-08-11 21:13:47 +0530

    whyred: drop low end media codec
    Change-Id: I457428d5c92f52fe2fc82b994cb015f9c67e2b9f
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- device.mk
+++ device.mk
@@ -104,7 +104,8 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+	$(TOPDIR)frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # ANT+
 PRODUCT_PACKAGES += \
