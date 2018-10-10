199613c279c0f36da36fa7f38cc0ddee9764198d
felix-development <felixprivateskonto@gmail.com>
2018-10-10 07:17:37 +0000

    whyred: bring up for RR-O

--- rr.mk
+++ rr.mk
@@ -22,14 +22,13 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := rr_whyred
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo rr_whyred-userdebug
