9d736606d5e332d02de2c5c28361ea7f124e7f5b
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    whyred: Correct TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- properties.mk
+++ properties.mk
@@ -28,7 +28,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.faceunlock,com.android.camera
-
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.sf.lcd_density=440
