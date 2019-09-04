317ba0b7f8a9df03d181cabd07cc0e424916ad3f
Davide Garberi <dade.garberi@gmail.com>
2019-09-04 23:30:15 +0200

    tulip: Use ifeq instead of ifneq for device inclusion

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter tulip,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),tulip)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
