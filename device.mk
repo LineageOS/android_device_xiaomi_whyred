95bb82b5f60a93a292e96c23089e2b51326080d5
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-08-27 00:50:44 +0530

    whyred: Add sdm660-common to soong namespaces for libinit
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    Change-Id: I19ba44788bc299c263ed2c95be73c6b76741c5a8

--- device.mk
+++ device.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
