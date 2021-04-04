33edd03570b864be3bc3b92ea37d3d5a7ab9941a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-05 01:24:35 +0200

    whyred: Update blobs to V12.0.2.0.PEIMIXM
    Change-Id: I57b001c0310d33827738582093b7cef39142ac1e

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -36,6 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+    PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 V12.0.2.0.PEIMIXM release-keys"
 
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+BUILD_FINGERPRINT := xiaomi/whyred/whyred:9/PKQ1.180904.001/V12.0.2.0.PEIMIXM:user/release-keys
