2c9858529219bfe63ae8176af2a695091dce5432
malfunctixn <malfunctixn@gmail.com>
2021-04-26 09:47:26 +0530

    Include lineage overlays

--- device.mk
+++ device.mk
@@ -67,7 +67,8 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-evolution
+    $(DEVICE_PATH)/overlay-evolution \
+    $(DEVICE_PATH)/overlay-lineage
 
 # Ramdisk
 PRODUCT_PACKAGES += \
