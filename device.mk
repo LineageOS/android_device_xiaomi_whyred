1636672180d973681a3c7b1eb6de67aefbaf4170
malfunctixn <malfunctixn@gmail.com>
2021-04-26 09:36:48 +0530

    Kang some overlays

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-evolution
 
 # Ramdisk
 PRODUCT_PACKAGES += \
