313432fcfe5d0870e73be538764c602ddabc0760
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-13 14:18:32 +0200

    whyred: Move to common IR impl
    Change-Id: If984976fe0eea957a4e88a0442d8bb4dd0db01b0

--- device.mk
+++ device.mk
@@ -20,12 +20,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 # Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+BOARD_HAVE_IR := true
 
 # FM
 BOARD_HAVE_QCOM_FM := true
