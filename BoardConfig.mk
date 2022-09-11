fd17de0342a03c8ee5eb27e46f3213fed8483949
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-11 16:01:40 +0200

    whyred: Use kernel config fragment
    Change-Id: If6a3eb00e358d0c5eeb610aacb288caf34e27e13

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG := whyred_defconfig
+TARGET_KERNEL_CONFIG += vendor/xiaomi/whyred.config
 
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
