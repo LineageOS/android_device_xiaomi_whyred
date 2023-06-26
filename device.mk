9da62c2c73dfab93beecec9201975a78ef0393e2
Michael Bestas <mkbestas@lineageos.org>
2023-06-26 05:25:27 +0300

    whyred: Switch to FBE
    Change-Id: I7c71c0e43128fd71c802598b3853009265a8a9c6

--- device.mk
+++ device.mk
@@ -30,10 +30,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
